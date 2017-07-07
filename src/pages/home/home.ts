e13edffebec1fc4b15dc113d2ba426256faafd4a
iddiaz <diazdiazivan@gmail.com>
2017-07-08 00:00:00 +0200

    implementacion logica de list-sliding y delete

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,57 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+
+import { ANIMALES } from './../../data/data.animales';
+import { Animal } from './../../interfaces/animal.interface';
+
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  animales: Animal[] = [];
+  audio = new Audio();
+  audioTiempo: any;
 
+  constructor() {
+    this.animales = ANIMALES.splice(0); //clonamos el objeto de referencia para no sobreescribirlo
+  }
+
+  reproducir( animal: Animal ){
+    console.log(animal);
+    // let audio = new Audio();
+
+    this.pausar_audio(animal);
+    if(animal.reproduciendo){
+      animal.reproduciendo = false;
+      return;
+    }
+
+    this.audio.src = animal.audio;
+    this.audio.load();
+    this.audio.play();
+  
+    this.audioTiempo = setTimeout(()=> animal.reproduciendo = false, animal.duracion * 1000)
+
+    animal.reproduciendo = true;
+  }
+
+  private pausar_audio( animalSel: Animal){
+    clearTimeout( this.audioTiempo );
+    this.audio.pause();
+    this.audio.currentTime = 0;
+
+    for( let animal of this.animales ){
+      if( animal.nombre != animalSel.nombre ){
+
+      }
+    }
+
+  }
+
+  borrarAnimal(idx:number){
+    this.animales.splice(idx, 1);
   }
 
 }
